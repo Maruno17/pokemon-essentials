61ad59afa63dfab284f0c7373d54318c10267abd
Joni Savolainen <joni@jonisavo.xyz>
2020-09-20 21:24:05 +0300

    Create 009_Pokemon_Deprecated.rb

--- Data/Scripts/016_Pokemon/001_Pokemon.rb
+++ Data/Scripts/016_Pokemon/001_Pokemon.rb
@@ -1120,23 +1120,3 @@ class Pokemon
     end
   end
 end
-
-#===============================================================================
-# Deprecated classes & methods
-#===============================================================================
-
-# @deprecated Use {Pokemon} instead. PokeBattle_Pokemon has been turned into an alias
-#   and is slated to be removed in vXX.
-class PokeBattle_Pokemon; end
-
-PokeBattle_Pokemon = Pokemon
-
-# (see Pokemon#initialize)
-# @deprecated Use +Pokemon.new+ instead. This method and its aliases are
-#   slated to be removed in vXX.
-def pbNewPkmn(species, level, owner = $Trainer, withMoves = true)
-  Kernel.echoln("WARN: pbNewPkmn and its aliases are deprecated and slated to be removed in Essentials vXX")
-  return Pokemon.new(species, level, owner, withMoves)
-end
-alias pbGenPkmn pbNewPkmn
-alias pbGenPoke pbNewPkmn
