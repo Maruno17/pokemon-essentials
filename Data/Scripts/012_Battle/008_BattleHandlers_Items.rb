30ad788ce5f1cd7e3b0f449721405687e2c8178d
Erwan Beurier <erwan.beurier@telecom-bretagne.eu>
2020-11-15 20:56:33 +0100

    Eject Pack doesn't activate on Parting Shot (and switching moves)

--- Data/Scripts/012_Battle/008_BattleHandlers_Items.rb
+++ Data/Scripts/012_Battle/008_BattleHandlers_Items.rb
@@ -1654,6 +1654,8 @@ BattleHandlers::ItemOnStatLoss.add(:EJECTPACK,
   proc { |item,battler,user,move,switched,battle|
     next if battle.pbAllFainted?(battler.idxOpposingSide)
     next if !battle.pbCanChooseNonActive?(battler.index)
+	next if move.function=="0EE" # U-Turn, Volt-Switch, Flip Turn
+	next if move.function=="151" # Parting Shot
     battle.pbCommonAnimation("UseItem",battler)
     battle.pbDisplay(_INTL("{1} is switched out with the {2}!",battler.pbThis,battler.itemName))
     battler.pbConsumeItem(true,false)
