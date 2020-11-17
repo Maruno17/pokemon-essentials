4cc355a1d60c103f7028f82959aa8d01ca5d7f1b
Erwan Beurier <erwan.beurier@telecom-bretagne.eu>
2020-11-17 09:52:27 +0100

    Neutralizing Gas adjustments: deactivation on switching out

--- Data/Scripts/012_Battle/003_Battle/012_Battle_Phase_EndOfRound.rb
+++ Data/Scripts/012_Battle/003_Battle/012_Battle_Phase_EndOfRound.rb
@@ -676,19 +676,37 @@ class PokeBattle_Battle
     @field.effects[PBEffects::FairyLock]   -= 1 if @field.effects[PBEffects::FairyLock]>0
     @field.effects[PBEffects::FusionBolt]  = false
     @field.effects[PBEffects::FusionFlare] = false
+	
+	# Neutralizing Gas
+	pbCheckNeutralizingGas
+	
+    @endOfRound = false
+  end
+  
+  
+  def pbCheckNeutralizingGas(battler=nil)
+    # Battler = the battler to switch out. 
+	# Should be specified when called from pbAttackPhaseSwitch
+	# Should be nil when called from pbEndOfRoundPhase
+    return if !@field.effects[PBEffects::NeutralizingGas]
+    return if battler && (!isConst?(battler.ability,PBAbilities,:NEUTRALIZINGGAS) || 
+		battler.effects[PBEffects::GastroAcid])
     hasabil=false
-    neutralactive=@field.effects[PBEffects::NeutralizingGas]
     eachBattler {|b|
       next if !b || b.fainted?
+	  next if battler && b.index == battler.index 
+	  # if specified, the battler will switch out, so don't consider it.
       # neutralizing gas can be blocked with gastro acid, ending the effect.
       if isConst?(b.ability,PBAbilities,:NEUTRALIZINGGAS) && !b.effects[PBEffects::GastroAcid]
         hasabil=true; break
       end
     }
-    if !hasabil && neutralactive
+    if !hasabil
       @field.effects[PBEffects::NeutralizingGas] = false
-      priority.each { |b| b.pbEffectsOnSwitchIn }
+      pbPriority(true).each { |b| 
+	    next if battler && b.index == battler.index
+	    b.pbEffectsOnSwitchIn
+	  }
     end
-    @endOfRound = false
-  end
+  end 
 end
