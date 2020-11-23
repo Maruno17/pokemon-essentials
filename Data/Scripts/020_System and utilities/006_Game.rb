58c565fe90a320a42aa35ae1c42c43d409e90c82
Joni Savolainen <joni@savolainen.io>
2020-11-23 19:08:17 +0200

    Ensure save file safety before saving/loading

--- Data/Scripts/020_System and utilities/006_Game.rb
+++ Data/Scripts/020_System and utilities/006_Game.rb
@@ -3,23 +3,30 @@ module Game
   # Saves the game. Returns whether the operation was successful.
   # @return [Boolean] whether the operation was successful
   def self.save
-    $game_system.save_count += 1 # FIXME: Right now, the save count is incremented even if saving fails
+    return false unless File.writable?(SaveData::FILE_PATH)
+
+    $game_system.save_count += 1
     begin
       SaveData.save_to_file(SaveData::FILE_PATH)
     rescue IOError, SystemCallError
+      $game_system.save_count -= 1 # TODO: I feel like this is a bit of a hack. Remove?
       return false
     end
+
     return true
   end
 
   # Loads the game. Returns whether the operation was successful.
   # @return [Boolean] whether the operation was successful
   def self.load
+    return false unless File.readable?(SaveData::FILE_PATH)
+
     begin
       data = SaveData.load_from_file(SaveData::FILE_PATH)
     rescue IOError, SystemCallError
       return false
     end
+
     data = SaveData.to_hash_format(data) if data.is_a?(Array)
     # TODO: Handle save data conversion here
     SaveData.load_values(data)
