73e9030a9ec42d3d5fa88f89987168b29d248a34
Joni Savolainen <joni@savolainen.io>
2021-01-05 13:59:26 +0200

    Add Game.initialize and Game.start_new

--- Data/Scripts/020_System and utilities/006_Game.rb
+++ Data/Scripts/020_System and utilities/006_Game.rb
@@ -1,5 +1,13 @@
 # The Game module contains methods for saving and loading the game.
 module Game
+  def self.initialize
+    $PokemonTemp   = PokemonTemp.new
+    $game_temp     = Game_Temp.new
+    $game_system   = Game_System.new
+    $data_system   = pbLoadRxData('Data/System')
+    $PokemonSystem = PokemonSystem.new if $PokemonSystem.nil?
+  end
+
   # Saves the game. Returns whether the operation was successful.
   # @param save_file [String] the save file path ({SaveData::FILE_PATH} by default)
   # @return [Boolean] whether the operation was successful
@@ -11,7 +19,7 @@ module Game
     begin
       SaveData.save_to_file(save_file)
     rescue IOError, SystemCallError
-      $game_system.save_count -= 1 # TODO: I feel like this is a bit of a hack. Remove?
+      $game_system.save_count -= 1
       return false
     end
 
@@ -19,6 +27,7 @@ module Game
   end
 
   # Loads the game. Returns whether the operation was successful.
+  # TODO: Is this method necessary? It may be removed soon.
   # @param save_file [String] the save file path ({SaveData::FILE_PATH} by default)
   # @return [Boolean] whether the operation was successful
   # @raise [SaveData::InvalidValueError] if an invalid value is being loaded
@@ -35,5 +44,19 @@ module Game
     # TODO: Handle save data conversion here
     SaveData.load_values(data)
     return true
+  end
+
+  def self.start_new
+    if $game_map && $game_map.events
+      $game_map.events.each_value { |event| event.clear_starting }
+    end
+    $game_temp.common_event_id = 0 if $game_temp
+    $PokemonTemp.begunNewGame = true
+    $scene = Scene_Map.new
+    SaveData.load_new_game_values
+    $game_player.moveto($data_system.start_x, $data_system.start_y)
+    $game_player.refresh
+    $game_map.autoplay
+    $game_map.update
   end
 end
