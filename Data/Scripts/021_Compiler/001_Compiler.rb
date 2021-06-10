f53567b37428e5885566e464708404050aa67f7a
ENLSLive <eliasnikolai7@gmail.com>
2021-06-10 18:49:22 +0200

    Compiler now loads messages correctly
    Compiler no longer loads messages.dat BEFORE compiling, but rather after. This has also been tested with multiple languages, and seems to work without any issues.

--- Data/Scripts/021_Compiler/001_Compiler.rb
+++ Data/Scripts/021_Compiler/001_Compiler.rb
@@ -677,9 +677,6 @@ module Compiler
   #=============================================================================
   def compile_all(mustCompile)
     FileLineData.clear
-    if (!$INEDITOR || Settings::LANGUAGES.length < 2) && safeExists?("Data/messages.dat")
-      MessageTypes.loadMessageFile("Data/messages.dat")
-    end
     if mustCompile
       echoln _INTL("*** Starting full compile ***")
       echoln ""
@@ -732,8 +729,11 @@ module Compiler
       echoln _INTL("*** Finished full compile ***")
       echoln ""
       System.reload_cache
-      MessageTypes.loadMessageFile("Data/messages.dat")
+      if (!$INEDITOR || Settings::LANGUAGES.length < 2) && safeExists?("Data/messages.dat")
+        MessageTypes.loadMessageFile("Data/messages.dat")
+      end
     end
+
     pbSetWindowText(nil)
   end
 
