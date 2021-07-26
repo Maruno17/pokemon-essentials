88bb57782a4e36e2ab51ac60b68cd6b3900888b3
Maruno17 <serialcolour@hotmail.com>
2021-07-26 22:00:37 +0100

    Update 004_AI_Move.rb

--- Data/Scripts/011_Battle/004_AI/004_AI_Move.rb
+++ Data/Scripts/011_Battle/004_AI/004_AI_Move.rb
@@ -191,17 +191,13 @@ class PokeBattle_AI
         score -= 80 if miss
       end
       # Pick a good move for the Choice items
-      if user.hasActiveItem?([:CHOICEBAND,:CHOICESPECS,:CHOICESCARF])
+      if user.hasActiveItem?([:CHOICEBAND,:CHOICESPECS,:CHOICESCARF]) ||
+         user.hasActiveAbility?(:GORILLATACTICS)
         if move.baseDamage>=60;     score += 60
         elsif move.damagingMove?;   score += 30
         elsif move.function=="0F2"; score += 70   # Trick
         else;                       score -= 60
         end
-      end
-      elsif user.hasActiveAbility?(:GORILLATACTICS)
-        if move.baseDamage>=60;     score += 60
-        elsif move.damagingMove?;   score += 30
-        else;                       score -= 60
       end
       # If user is asleep, prefer moves that are usable while asleep
       if user.status == :SLEEP && !move.usableWhenAsleep?
