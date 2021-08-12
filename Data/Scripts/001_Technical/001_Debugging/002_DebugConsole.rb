19535b81607aca2aad4d364e7afaeab41e560dc9
Maruno17 <serialcolour@hotmail.com>
2021-08-12 20:00:42 +0100

    Fixed Symbiosis, fixed Roost not roosting, fixed Normalize not boosting damage, fixed crash in Bug Catching Contest, fixed old format moves.txt compiling, added cache size text to console

--- Data/Scripts/001_Technical/001_Debugging/002_DebugConsole.rb
+++ Data/Scripts/001_Technical/001_Debugging/002_DebugConsole.rb
@@ -4,6 +4,7 @@
 module Console
   def self.setup_console
     return unless $DEBUG
+    echoln "GPU Cache Max: #{Bitmap.max_size}"
     echoln "--------------------------------"
     echoln "#{System.game_title} Output Window"
     echoln "--------------------------------"
