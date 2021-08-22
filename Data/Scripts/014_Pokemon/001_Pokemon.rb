ca5436e78d68fa7e6b2175affc52259824ef68d0
Golisopod-User <pkmnmonsieur@gmail.com>
2021-08-22 07:45:00 +0530

    Fixed crash related to Pokemon with forms in the Load Screen

--- Data/Scripts/014_Pokemon/001_Pokemon.rb
+++ Data/Scripts/014_Pokemon/001_Pokemon.rb
@@ -152,7 +152,7 @@ class Pokemon
     @ability = nil
     MultipleForms.call("onSetForm", self, value, oldForm)
     calc_stats
-    $Trainer.pokedex.register(self)
+    $Trainer.pokedex.register(self) if $Trainer
   end
 
   # The same as def form=, but yields to a given block in the middle so that a
