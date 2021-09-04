39714c97ef5c95d98a7127b24e68bb31b9929683
infinitefusion <pkmninfinitefusion@gmail.com>
2021-09-04 15:41:44 -0400

    fixed npx trade

--- Data/Scripts/049_Compatibility/PBSpecies.rb
+++ Data/Scripts/049_Compatibility/PBSpecies.rb
@@ -7,5 +7,24 @@ module PBSpecies
   def PBSpecies.getName(species)
     return GameData::Species.get(species).real_name
   end
+
+#In some places, pokemon are instanciated as PBSpecies::NAME in wild battles, trades, etc. which doesn't work anymore.
+  # Instead of replacing every instance in every map, this is a workaround to make it work without changing the events.
+  CLEFAIRY = :CLEFAIRY
+  ONIX =:ONIX
+  DUGTRIO = :DUGTRIO
+  VOLTORB = :VOLTORB
+  ELECTRODE = :ELECTRODE
+  B101H135 = :B101H135
+  B100H101 = :B100H101
+  B101H26 = :B101H26
+  ENTEI = :ENTEI
+  PIDGEOTTO = :PIDGEOTTO
+  FEAROW =:FEAROW
+  SPEAROW =:SPEAROW
+  B18H18 = :B18H18
+  B245H243 = :B245H243
+  CHARMANDER = :CHARMANDER
+
 end
 
