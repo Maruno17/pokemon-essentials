a89d552f8e5db0a3ab127c9a5ca82fd1a899eea6
Nathan-mv <67521199+Nathan-mv@users.noreply.github.com>
2021-12-13 19:24:46 -0300

    Update Boxes (#144)

--- Data/Scripts/001_Settings.rb
+++ Data/Scripts/001_Settings.rb
@@ -181,7 +181,7 @@ module Settings
   #=============================================================================
 
   # The number of boxes in Pokémon storage.
-  NUM_STORAGE_BOXES = 30
+  NUM_STORAGE_BOXES = 40
   # Whether putting a Pokémon into Pokémon storage will heal it. IF false, they
   # are healed by the Recover All: Entire Party event command (at Poké Centers).
   HEAL_STORED_POKEMON = true
