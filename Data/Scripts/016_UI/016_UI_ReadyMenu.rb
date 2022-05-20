ce5d68a904dbc27ea81b095c2d624a732e4293d9
Maruno17 <serialcolour@hotmail.com>
2022-05-20 19:11:12 +0100

    Fixed incorrect Pokémon icons shown in Ready Menu if there are eggs in the party

--- Data/Scripts/016_UI/016_UI_ReadyMenu.rb
+++ Data/Scripts/016_UI/016_UI_ReadyMenu.rb
@@ -305,8 +305,8 @@ def pbUseKeyItem
            :WATERFALL, :WHIRLPOOL]
   real_moves = []
   moves.each do |move|
-    $player.pokemon_party.each_with_index do |pkmn, i|
-      next if !pkmn.hasMove?(move)
+    $player.party.each_with_index do |pkmn, i|
+      next if pkmn.egg? || !pkmn.hasMove?(move)
       real_moves.push([move, i]) if pbCanUseHiddenMove?(pkmn, move, false)
     end
   end
