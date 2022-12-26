2acca429e369a3db04a837410772e97fb3317c19
chardub <pkmninfinitefusion@gmail.com>
2022-12-26 18:33:37 -0500

    Adds support for shinies in online wondertrade

--- Data/Scripts/050_AddOns/OnlineWondertrade.rb
+++ Data/Scripts/050_AddOns/OnlineWondertrade.rb
@@ -1,7 +1,4 @@
 class OnlineWondertrade
-
-
-
   def pbWonderTrade()
     givenPokemon = selectPokemonToGive
     return if givenPokemon == nil
@@ -11,6 +8,8 @@ class OnlineWondertrade
     if response[:status] == 200
       body = HTTPLite::JSON.parse(response[:body])
       doTrade(body)
+    else
+      pbMessage("Could not find a trading partner...")
     end
     rescue MKXPError
       pbMessage("There was an error while sending your Pokémon...")
@@ -26,16 +25,37 @@ class OnlineWondertrade
     receivedPokemonTrainerName = receivedData["trainer_name"]
     receivedPokemonTrainerGender = receivedData["trainer_gender"].to_i
 
+    is_head_shiny = receivedData["head_shiny"]
+    is_body_shiny = receivedData["body_shiny"]
+    is_debug_shiny = receivedData["debug_shiny"]
+
     newpoke = pbStartTrade(pbGet(1), receivedPokemonSpecies, receivedPokemonName, receivedPokemonTrainerName, receivedPokemonTrainerGender, true) # Starts the trade
     newpoke.owner=Pokemon::Owner.new(receivedPokemonTrainerId.to_i,receivedPokemonOT,2,2)
     newpoke.level=receivedPokemonLevel
+
+    if is_head_shiny || is_body_shiny
+      newpoke.shiny=true
+      newpoke.head_shiny=is_head_shiny
+      newpoke.body_shiny = is_body_shiny
+      if is_debug_shiny
+        newpoke.debug_shiny=false
+        newpoke.natural_shiny=true
+      else
+        newpoke.debug_shiny=true
+        newpoke.natural_shiny=false
+      end
+    end
     newpoke.calc_stats
   end
 
   def selectPokemonToGive
     pbChoosePokemon(1, 2, # Choose eligable pokemon
                     proc {
-                      |poke| !poke.egg? && !(poke.isShadow?)
+                      |poke| !poke.egg? &&
+                        !(poke.isShadow?) &&
+                        poke.isFusion? &&
+                      customSpriteExists(poke.species) #&&
+                      #  !poke.debug_shiny
                     })
     poke = $Trainer.party[pbGet(1)]
 
@@ -48,6 +68,7 @@ class OnlineWondertrade
 
   # @param [Pokemon] givenPokemon
   def buildWondertradeQueryJson(givenPokemon)
+    isDebugShiny = givenPokemon.debug_shiny || !givenPokemon.natural_shiny
     postData = {
       "trainer_name" => $Trainer.name,
       "trainer_gender" => $Trainer.gender,
@@ -58,6 +79,11 @@ class OnlineWondertrade
       "nickname" => givenPokemon.name,
       "original_trainer_name" => givenPokemon.owner.name,
       "original_trainer_id" => givenPokemon.owner.id.to_s,
+      "body_shiny" => givenPokemon.body_shiny == nil ? false : givenPokemon.body_shiny,
+      "head_shiny" => givenPokemon.head_shiny == nil ? false : givenPokemon.head_shiny,
+      "debug_shiny" => isDebugShiny,
+      "original_trainer_id" => givenPokemon.owner.id.to_s,
+
     }
     return HTTPLite::JSON.stringify(postData)
   end
