3575d44a9b8757614af74d8c307b0cd20f1eabab
ENLS <78400667+ENLSLive@users.noreply.github.com>
2023-05-28 20:32:19 +0200

    Update 015_UI_Options.rb

--- Data/Scripts/016_UI/015_UI_Options.rb
+++ Data/Scripts/016_UI/015_UI_Options.rb
@@ -23,8 +23,8 @@ class PokemonSystem
     @screensize  = (Settings::SCREEN_SCALE * 2).floor - 1   # 0=half size, 1=full size, 2=full-and-a-half size, 3=double size
     @language    = 0     # Language (see also Settings::LANGUAGES in script PokemonSystem)
     @runstyle    = 0     # Default movement speed (0=walk, 1=run)
-    @bgmvolume   = 50   # Volume of background music and ME
-    @sevolume    = 50   # Volume of sound effects
+    @bgmvolume   = 50    # Volume of background music and ME
+    @sevolume    = 50    # Volume of sound effects
     @textinput   = 0     # Text input mode (0=cursor, 1=keyboard)
   end
 end
