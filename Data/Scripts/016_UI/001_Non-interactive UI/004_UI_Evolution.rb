aa6f869cf88ef3f5d35af73ad34afcbab3bbb15a
Maruno17 <serialcolour@hotmail.com>
2026-05-26 17:43:07 +0100

    Fixed not learning evo moves upon evolution

--- Data/Scripts/016_UI/001_Non-interactive UI/004_UI_Evolution.rb
+++ Data/Scripts/016_UI/001_Non-interactive UI/004_UI_Evolution.rb
@@ -228,7 +228,7 @@ class PokemonEvolutionScene
     moves_to_learn = []
     movelist = @pokemon.getMoveList
     movelist.each do |i|
-      next if i[0] <= 0 || i[0] != @pokemon.level   # 0 is "learn upon evolution"
+      next if ![0, @pokemon.level].include?(i[0])   # 0 is "learn upon evolution"
       moves_to_learn.push(i[1])
     end
     # Show Pokédex entry for new species if it hasn't been owned before
